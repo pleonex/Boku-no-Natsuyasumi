087baf836b7d5630a9b57d368edf5bd0df9bb439
Benito Palacios Sánchez <benito356@gmail.com>
2014-12-17 01:03:17 +0100

    Implemented pack type

---
+++ Bokuract/Bokuract/Pack.cs
@@ -0,0 +1,74 @@
+﻿//
+//  Pack.cs
+//
+//  Author:
+//       Benito Palacios Sánchez <benito356@gmail.com>
+//
+//  Copyright (c) 2014 Benito Palacios Sánchez
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+using Mono.Addins;
+using Libgame;
+using Libgame.IO;
+
+namespace Bokuract
+{
+	[Extension]
+	public class Pack : Format
+	{
+		public override string FormatName {
+			get { return "Boku1.Pack"; }
+		}
+
+		public override void Read(DataStream strIn)
+		{
+			DataReader reader = new DataReader(strIn);
+
+			int numFiles = reader.ReadInt32();
+			for (int i = 0; i < numFiles; i++) {
+				strIn.Seek(4 + i * 0xC, SeekMode.Origin);
+
+				uint offset     = reader.ReadUInt32();
+				uint size       = reader.ReadUInt32();
+				uint nameOffset = reader.ReadUInt32();
+				strIn.Seek(nameOffset, SeekMode.Origin);
+				string filename = reader.ReadString();
+
+				DataStream fileStream = new DataStream(strIn, offset, size);
+				this.File.AddFile(new GameFile(filename, fileStream));
+			}
+		}
+
+		public override void Write(DataStream strOut)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void Import(params DataStream[] strIn)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void Export(params DataStream[] strOut)
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override void Dispose(bool freeManagedResourcesAlso)
+		{
+		}
+	}
+}
+
---
+++ Bokuract/Bokuract/PackValidation.cs
@@ -0,0 +1,65 @@
+﻿//
+//  PackValidation.cs
+//
+//  Author:
+//       Benito Palacios Sánchez <benito356@gmail.com>
+//
+//  Copyright (c) 2014 Benito Palacios Sánchez
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+using Libgame;
+using Mono.Addins;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Bokuract
+{
+	[Extension]
+	public class PackValidation : FormatValidation
+	{
+		public override Type FormatType {
+			get { return typeof(Pack); }
+		}
+
+		protected override string[] GuessDependencies(GameFile file)
+		{
+			return null;
+		}
+
+		protected override object[] GuessParameters(GameFile file)
+		{
+			return null;
+		}
+
+		protected override ValidationResult TestByData(Libgame.IO.DataStream stream)
+		{
+			return ValidationResult.Invalid;
+		}
+
+		protected override ValidationResult TestByRegexp(string filepath, string filename)
+		{
+			string dirname = Path.GetDirectoryName(filepath);
+			return dirname.EndsWith("cdimg0.img/map/gz") ?
+				ValidationResult.Sure : ValidationResult.No;
+		}
+
+		protected override ValidationResult TestByTags(IDictionary<string, object> tags)
+		{
+			return ((string)tags["_Device_"] == "PSP") ?
+				ValidationResult.CouldBe : ValidationResult.No;
+		}
+	}
+}
+
